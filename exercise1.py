f2d623107e99105f52822841c1f5ba64d8a8391b
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-10-01 13:19:38 +0800

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,7 @@
 
 '''
-List Comprehension
+creates a new list containing only the numbers from the original list 
+that are divisible by 3 and greater than 10.
 '''
 numbers = [12, 5, 9, 21, 14, 7]
 filtered = [n __________ n % 3 == 0 __________ n > 10]
